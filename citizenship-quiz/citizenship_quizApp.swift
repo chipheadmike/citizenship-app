d0ca06bd6e409d34f78fd787d1c70d91dac2f294
Mike Williams <michael.williams@jaygroup.com>
2025-03-02 08:09:35 -0500

    Initial Commit

---
+++ citizenship-quiz/citizenship_quizApp.swift
@@ -0,0 +1,17 @@
+//
+//  citizenship_quizApp.swift
+//  citizenship-quiz
+//
+//  Created by Mike Williams on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct citizenship_quizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ citizenship-quizTests/citizenship_quizTests.swift
@@ -0,0 +1,17 @@
+//
+//  citizenship_quizTests.swift
+//  citizenship-quizTests
+//
+//  Created by Mike Williams on 3/2/25.
+//
+
+import Testing
+@testable import citizenship_quiz
+
+struct citizenship_quizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
